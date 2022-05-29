b2b280acd47be68be08f4fa2af9c874bd04ae245
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-05-29 09:55:18 -0700

    - created Koin di module to specify how to provide handle to the DB - created ConfigureKoin plugin

---
+++ src/main/kotlin/com/example/di/KoinModule.kt
@@ -0,0 +1,14 @@
+package com.example.di
+
+import com.env.EnvConfig
+import org.koin.dsl.module
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+val koinModule = module {
+    single {
+        KMongo.createClient()
+            .coroutine
+            .getDatabase(EnvConfig.DATABASE_NAME)
+    }
+}
