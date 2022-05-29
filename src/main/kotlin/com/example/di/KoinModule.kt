1731ef398b94cf67b3ffcb5ec544fb14a6fb24ce
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-05-29 10:09:02 -0700

    - created UserDataSource.kt interface to define crud operations for accessing the user document from mongo - created UserDataSourceImpl.kt - updated KoinModule.kt to add provider for the UserDataSource.kt

--- src/main/kotlin/com/example/di/KoinModule.kt
+++ src/main/kotlin/com/example/di/KoinModule.kt
@@ -1,6 +1,8 @@
 package com.example.di
 
 import com.env.EnvConfig
+import com.example.data.repository.UserDataSourceImpl
+import com.example.domain.repository.UserDataSource
 import org.koin.dsl.module
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
@@ -10,5 +12,8 @@ val koinModule = module {
         KMongo.createClient()
             .coroutine
             .getDatabase(EnvConfig.DATABASE_NAME)
+    }
+    single<UserDataSource> {
+        UserDataSourceImpl(get())
     }
 }
